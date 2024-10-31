813200e6c0a42bdcd6ef1570e4c64c6c714f5b66
Wallace Vilela <wallace.vilela9@gmail.com>
2024-10-31 19:42:54 -0300

    Create project

---
+++ src/main/java/wvsdev/twitterapp/TwitterAppApplication.java
@@ -0,0 +1,13 @@
+package wvsdev.twitterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wvsdev/twitterapp/TwitterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package wvsdev.twitterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
