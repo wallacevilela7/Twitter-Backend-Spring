d4462d5e2d4b2040c841bc0d1cd430037dceb997
Wallace Vilela <wallace.vilela9@gmail.com>
2024-11-01 15:11:17 -0300

    feat: Configuração básica do Spring Security

---
+++ src/main/java/wvsdev/twitterapp/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package wvsdev.twitterapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.
+                authorizeRequests(authorize -> authorize.anyRequest().authenticated())
+                .csrf(csrf -> csrf.disable())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
